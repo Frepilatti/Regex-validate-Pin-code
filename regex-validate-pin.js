abe23a3a648672da03aa6721e95816c5b92e4da4
frepilatti <frepilatti@hotmail.com>
2020-07-30 17:39:04 -0300

    first commit

---
+++ regex-validate-pin.js
@@ -0,0 +1,13 @@
+function validatePIN (pin) {
+
+    if(pin.match(/^[0-9]+$/) != null && (pin.length == 4 || pin.length == 6)){
+   
+        return true;
+      
+    } else {
+      
+      return false;
+      
+    }
+  
+  }
